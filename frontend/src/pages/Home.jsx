a4d22ccfc53209aae7ab3f55ab411a4baaf40157
木下　晴翔 <241205064@ccmailg.meijo-u.ac.jp>
2025-11-29 22:45:37 +0900

    chore: JSでのバックアップ

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,17 +8,16 @@ function Home() {
     <Box bg="gray.100" minH="100vh" py={10} px={4}>{/* 全体の背景Box */}
       
       <Container /* 画面用の白いコンテナ */
-        maxW="480px"  
-        bg="white"         
-        minH="80vh"        
-        borderRadius="2xl" 
-        boxShadow="xl"     
+        maxW="480px"
+        bg="white"
+        minH="80vh"
+        borderRadius="2xl"
+        boxShadow="xl"
         p={0}              
         overflow="hidden"
       >
         <Box bg="pink.400" p={6} textAlign="center">{/* ヘッダーBox */}
           <Heading color="white" size="lg">TomoTune</Heading>
-          <Text fontSize="sm" color="whiteAlpha.900" mt={1}>音楽でつながるSNS</Text>
         </Box>
 
         <Box p={6}>{/* メインコンテンツBox */}
