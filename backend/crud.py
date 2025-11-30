192299e40e0ae5c6f54d4e0a814e2b6aedbd238d
木下　晴翔 <241205064@ccmailg.meijo-u.ac.jp>
2025-11-30 18:01:12 +0900

    feat: ログイン機能(backend)

--- backend/crud.py
+++ backend/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func # 集計用(COUNTとか)
-import os
+import uuid
 from models import User, Song
 
 # --- 曲の操作 ---
@@ -14,6 +14,28 @@ def get_song_by_id(db: Session, song_id: int):
     return db.query(Song).filter(Song.id == song_id).first()
 
 # --- ユーザーの操作 ---
+# 名前からユーザーを探す
+def get_user_by_name(db: Session, name: str):
+    return db.query(User).filter(User.name == name).first()
+
+# IDからユーザーを探す
+def get_user_by_id(db: Session, user_id: str):
+    return db.query(User).filter(User.id == user_id).first()
+
+# 新しいユーザーを登録する
+def create_user(db: Session, name: str):
+    # UUID4 (ランダムなID) を生成して文字列にする
+    new_id = str(uuid.uuid4())
+    
+    new_user = User(
+        id=new_id,
+        name=name,
+        mbti=None
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
 
 def get_test_user(db: Session):
     """
